13bc4aa5a6abbd38694c3861da5ece318d7c0a34
manato-alt <cococara988@gmail.com>
2024-03-23 17:46:26 +0900

    add: Headerコンポーネントを作成

--- progline-app/src/App.js
+++ progline-app/src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
 import "./App.css";
+import Header from "./component/Header";
 
 function App() {
   return (
     <div className="App">
-      <h1 className="text-3xl font-bold underline">Hello World</h1>
-      <button className="btn btn-primary">Button</button>
+      <Header />
     </div>
   );
 }
