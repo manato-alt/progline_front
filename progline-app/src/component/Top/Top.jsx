33e9689d5f64dd8785cd41a88599a8cf647525b0
manato-alt <cococara988@gmail.com>
2024-04-02 15:26:36 +0900

    add:topページ用の画像を追加

--- progline-app/src/component/Top/Top.jsx
+++ progline-app/src/component/Top/Top.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import SearchForm from "./SearchForm";
 import UserList from "./UserList";
+import myImage from "../../images/iStock-1201580867.jpg";
 
 export default function Top() {
   return (
@@ -15,8 +16,18 @@ export default function Top() {
           </p>
         </div>
       </div>
-      <SearchForm />
-      <UserList />
+      <div>
+        <SearchForm />
+        <UserList />
+      </div>
+      <div class="mt-5">
+        <button class="bg-black text-white py-2 px-4 rounded-lg font-semibold transition-colors hover:bg-gray-700">
+          ログインして始める
+        </button>
+      </div>
+      <div class="w-1/2 mx-auto">
+        <img src={myImage} alt="イラスト画像" class="object-contain" />
+      </div>
     </div>
   );
 }
