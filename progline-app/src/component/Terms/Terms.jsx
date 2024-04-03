2081fcc64107b02b423cff826e5474c81d3624d5
manato-alt <cococara988@gmail.com>
2024-04-03 23:03:05 +0900

    add:React Routerを導入、TermsページのUIを作成

---
+++ progline-app/src/component/Terms/Terms.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import myImage from "../../images/toppage-removebg.jpg";
+
+export default function Terms() {
+  return (
+    <div>
+      <div className="bg-[#f8f9fb] flex justify-center items-center  pt-5 pl-5 pr-32">
+        <img
+          src={myImage}
+          alt="イラスト画像"
+          className="object-contain w-1/3"
+        />
+        <p className="text-3xl text-sky-500 font-bold">
+          技術の道しるべ、
+          <br />
+          IT特化の学習メモリアル
+        </p>
+      </div>
+      <div className="border flex justify-between p-5  my-5 mx-32">
+        <div>登録したカテゴリ</div>
+        <div>
+          <button className="bg-gray-300 py-2 px-4 rounded-lg font-semibold transition-colors hover:bg-gray-400">
+            カテゴリを追加
+          </button>
+        </div>
+      </div>
+      <div>グラフを表示</div>
+    </div>
+  );
+}
