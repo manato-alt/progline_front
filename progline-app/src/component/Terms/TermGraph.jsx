8ada0f3f87d18e4bd71d0705e55cbac49d6526e0
manato-alt <cococara988@gmail.com>
2024-05-14 01:03:08 +0900

    fix:エラー対応のためReact Chart.js 2をChart.jsライブラリに変更

--- progline-app/src/component/Terms/TermGraph.jsx
+++ progline-app/src/component/Terms/TermGraph.jsx
@@ -1,14 +1,14 @@
-import React, { useState, useEffect, useRef, useMemo } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import axios from "axios";
 import { auth } from "../../contexts/AuthContext";
 import { useAuthState } from "react-firebase-hooks/auth";
-import { Bar } from "react-chartjs-2";
 import Chart from "chart.js/auto";
 
 export default function TermGraph() {
   const [graphData, setGraphData] = useState(null);
   const [user] = useAuthState(auth);
   const chartRef = useRef(null);
+  const chartInstanceRef = useRef(null); // Chart.jsインスタンスを格納するためのref
 
   useEffect(() => {
     const fetchData = async () => {
@@ -29,82 +29,57 @@ export default function TermGraph() {
     fetchData();
   }, [user]);
 
-  const data = useMemo(
-    () => ({
-      labels: graphData ? graphData.map((data) => data.label) : [],
-      datasets: [
-        {
-          label: "コンテンツ数",
-          data: graphData ? graphData.map((data) => data.data) : [],
-          backgroundColor: [
-            "rgba(255, 99, 132, 0.2)",
-            "rgba(255, 159, 64, 0.2)",
-            "rgba(255, 205, 86, 0.2)",
-            "rgba(75, 192, 192, 0.2)",
-            "rgba(54, 162, 235, 0.2)",
-            "rgba(153, 102, 255, 0.2)",
-            "rgba(201, 203, 207, 0.2)",
-          ],
-          borderColor: [
-            "rgb(255, 99, 132)",
-            "rgb(255, 159, 64)",
-            "rgb(255, 205, 86)",
-            "rgb(75, 192, 192)",
-            "rgb(54, 162, 235)",
-            "rgb(153, 102, 255)",
-            "rgb(201, 203, 207)",
-          ],
-          borderWidth: 1,
-        },
-      ],
-    }),
-    [graphData]
-  );
-
-  const options = useMemo(
-    () => ({
-      maintainAspectRatio: false,
-      indexAxis: "y",
-      scales: {
-        y: {
-          ticks: {
-            beginAtZero: true,
-          },
-        },
-        x: {
-          ticks: {
-            stepSize: 1,
-          },
-        },
-      },
-    }),
-    []
-  );
-
   useEffect(() => {
     if (graphData && graphData.length > 0) {
-      if (chartRef && chartRef.current) {
-        chartRef.current.destroy();
+      if (chartInstanceRef.current) {
+        chartInstanceRef.current.destroy(); // 前のChartインスタンスを破棄する
       }
-      const ctx = document.getElementById("myChart")?.getContext("2d");
+      const ctx = chartRef.current.getContext("2d");
       if (ctx) {
-        chartRef.current = new Chart(ctx, {
+        chartInstanceRef.current = new Chart(ctx, {
           type: "bar",
-          data: data,
-          options: options,
+          data: {
+            labels: graphData.map((data) => data.label),
+            datasets: [
+              {
+                label: "コンテンツ数",
+                data: graphData.map((data) => data.data),
+                backgroundColor: [
+                  "rgba(255, 99, 132, 0.2)",
+                  "rgba(255, 159, 64, 0.2)",
+                  "rgba(255, 205, 86, 0.2)",
+                  "rgba(75, 192, 192, 0.2)",
+                  "rgba(54, 162, 235, 0.2)",
+                  "rgba(153, 102, 255, 0.2)",
+                  "rgba(201, 203, 207, 0.2)",
+                ],
+                borderColor: [
+                  "rgb(255, 99, 132)",
+                  "rgb(255, 159, 64)",
+                  "rgb(255, 205, 86)",
+                  "rgb(75, 192, 192)",
+                  "rgb(54, 162, 235)",
+                  "rgb(153, 102, 255)",
+                  "rgb(201, 203, 207)",
+                ],
+                borderWidth: 1,
+              },
+            ],
+          },
+          options: {
+            maintainAspectRatio: false,
+            indexAxis: "y", // y軸を使用することを明示
+            scales: {
+              y: {
+                beginAtZero: true,
+                stepSize: 1,
+              },
+            },
+          },
         });
       }
     }
-  }, [graphData, data, options]);
+  }, [graphData]);
 
-  return (
-    <Bar
-      id="myChart"
-      data={data}
-      options={options}
-      width={500}
-      height={800}
-      ref={chartRef}
-    />
-  );
+  return <canvas id="myChart" width={500} height={800} ref={chartRef} />;
 }
