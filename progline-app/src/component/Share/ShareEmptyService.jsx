4958f06b776b0e6874043cad2cb3754122212875
manato-alt <cococara988@gmail.com>
2024-05-23 22:34:31 +0900

    add: シェア用のカテゴリ、サービスが登録されていない時に表示するコンポーネントを作成

---
+++ progline-app/src/component/Share/ShareEmptyService.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { TiHome } from "react-icons/ti";
+
+export default function ShareEmptyService({ category, shareCode }) {
+  return (
+    <div className="flex flex-col items-center h-[92vh] pt-28 bg-[#f7f9fb] px-8">
+      {category && (
+        <>
+          <img
+            src={category.image_url}
+            alt="Logo"
+            className="w-24 h-24 rounded-full"
+          />
+          <p className="mb-8 font-bold text-2xl">{category.name}</p>
+        </>
+      )}
+      <p className="text-sm md:text-base mb-4">
+        登録されたサービスはありません。
+      </p>
+      <Link
+        to={`/shareTerms/${shareCode}`}
+        className="text-sm md:text-base flex items-center justify-center text-cyan-500 border border-cyan-500 bg-white hover:text-white hover:bg-cyan-500 py-2 px-4 rounded transition duration-300 ease-in-out"
+      >
+        カテゴリページに戻る
+      </Link>
+    </div>
+  );
+}
