0071a2f4c2a83efd6a8c145c5643df9104c9f243
manato-alt <cococara988@gmail.com>
2024-05-29 15:04:05 +0900

    add:レスポンスを待ちを視覚化させた

--- progline-app/src/component/TermDetail/contents/ContentTemplate.jsx
+++ progline-app/src/component/TermDetail/contents/ContentTemplate.jsx
@@ -9,11 +9,13 @@ export default function ContentTemplate({
 }) {
   const [errorMessages, setErrorMessages] = useState([]);
   const [url, setUrl] = useState("");
+  const [isLoading, setIsLoading] = useState(false); // ローディング状態を管理
 
   const handleSubmit = async (e) => {
     e.preventDefault();
 
     setErrorMessages([]);
+    setIsLoading(true); // ローディング開始
     try {
       const formData = new FormData();
       formData.append("url", url);
@@ -35,8 +37,8 @@ export default function ContentTemplate({
         setErrorMessages("登録中にエラーが発生しました");
       }
     } finally {
-      // フォーム送信後に入力値をクリアする
-      setUrl("");
+      setIsLoading(false); // ローディング終了
+      setUrl(""); // フォーム送信後に入力値をクリア
     }
   };
 
@@ -44,7 +46,6 @@ export default function ContentTemplate({
     <div>
       <form onSubmit={handleSubmit} className="flex flex-col">
         {errorMessages !== null &&
-          // errorMessages が文字列か配列かで処理を分岐
           (typeof errorMessages === "string" ? (
             <p className="text-red-500 mb-4">{errorMessages}</p>
           ) : (
@@ -69,10 +70,13 @@ export default function ContentTemplate({
           <button
             type="submit"
             className="bg-gray-300 py-2 px-10 rounded-lg font-semibold transition-colors hover:bg-gray-400 w-[fit-content]"
+            disabled={isLoading} // ローディング中はボタンを無効化
           >
-            登録
+            {isLoading ? "登録中..." : "登録"}
           </button>
         </div>
+        {isLoading && <div className="text-center mt-4">登録中です...</div>}{" "}
+        {/* ローディングインジケーター */}
       </form>
     </div>
   );
